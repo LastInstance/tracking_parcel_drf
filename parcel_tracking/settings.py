58134836bbfcb4199e3c2c0a23bf338d6c3450ef
LastInstance <123825454+LastInstance@users.noreply.github.com>
2024-01-06 20:52:48 +0200

    Update settings.py

--- parcel_tracking/settings.py
+++ parcel_tracking/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'parcels'
+    'parcels',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
